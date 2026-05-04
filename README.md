590abc7ba40ef3d583bcbee72b8eb36b33350f1b
Maciej Borkowski <borkowski.mac@gmail.com>
2026-05-04 09:21:37 +0200

    create template

---
+++ README.md
@@ -0,0 +1,2 @@
+1. Change project name in "config.yaml" in field "project_name", "description", "domain_name", "email"
+2. Run `init-repo.sh`
---
+++ init-repo.sh
@@ -0,0 +1,4 @@
+uv tool install "cookiecutter>=1.7.0"
+uvx cookiecutter https://github.com/cookiecutter/cookiecutter-django --no-input \
+  --config-file ./config.yaml \
+  --output-dir ../
